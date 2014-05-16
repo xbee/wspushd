0bd521b2174a37e6bd2c58cd2d7f5fcba7c96c97
huanglifeng <lifeng.huang@oumen.com>
2014-05-16 18:03:58 +0800

    some changes

--- test/screenticks.go
+++ test/screenticks.go
@@ -10,7 +10,7 @@ import (
 	"bytes"
 	"code.google.com/p/goprotobuf/proto"
 	"fmt"
-	opencv "github.com/lazywei/go-opencv/opencv"
+	// opencv "github.com/lazywei/go-opencv/opencv"
 	"github.com/vova616/screenshot"
 	"image/jpeg"
 	"log"
@@ -20,20 +20,20 @@ import (
 	"time"
 )
 
-func testcv() {
-	filename := "bert.png"
-	srcImg := opencv.LoadImage(filename)
-	if srcImg == nil {
-		panic("Loading Image failed")
-	}
-	defer srcImg.Release()
-	resized1 := opencv.Resize(srcImg, 400, 0, 0)
-	resized2 := opencv.Resize(srcImg, 300, 500, 0)
-	resized3 := opencv.Resize(srcImg, 300, 500, 2)
-	opencv.SaveImage("resized1.png", resized1, 0)
-	opencv.SaveImage("resized2.png", resized2, 0)
-	opencv.SaveImage("resized3.png", resized3, 0)
-}
+// func testcv() {
+// 	filename := "bert.png"
+// 	srcImg := opencv.LoadImage(filename)
+// 	if srcImg == nil {
+// 		panic("Loading Image failed")
+// 	}
+// 	defer srcImg.Release()
+// 	resized1 := opencv.Resize(srcImg, 400, 0, 0)
+// 	resized2 := opencv.Resize(srcImg, 300, 500, 0)
+// 	resized3 := opencv.Resize(srcImg, 300, 500, 2)
+// 	opencv.SaveImage("resized1.png", resized1, 0)
+// 	opencv.SaveImage("resized2.png", resized2, 0)
+// 	opencv.SaveImage("resized3.png", resized3, 0)
+// }
 
 func ss(t time.Time, ws *websocket.Conn) {
 	img, err := screenshot.CaptureScreen()
